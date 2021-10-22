35644a53ba0c9f35868172fe20b82fd45af07931
sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-10-21 21:30:38 -0300

    Arreglado formulario de posteo

--- src/views/Post.js
+++ src/views/Post.js
@@ -1,16 +1,18 @@
 import React, { useContext } from "react"
 import { Context } from '../store/appContext';
+import logo from '../image2.png';
 
 const Post = () => {
 
     const {actions} = useContext(Context)
 
 return(<div className="container" >
+        <img src={logo} className="img-fluid w-50 m-4" alt="kickstart"/><br></br><br></br>
 <form onSubmit={actions.handlePostSubmit} encType="multipart/form-data" method="POST">
     <div className="row">
         <div className="col-6 form-group p-4">
             <label>Nombre del producto</label>
-            <input type="text" name="producto" placeholder="Nombre del producto" className="form-control"/>
+            <input type="text" name="producto" placeholder="Producto" className="form-control"/>
         </div>
         <div className="col-6 form-group p-4">
             <label>Categoria</label>
@@ -18,7 +20,7 @@ return(<div className="container" >
         </div>
         <div className="col-6 form-group p-4">
             <label>Precio</label>
-            <input type="text" name="Precio" placeholder="Precio" className="form-control"/>
+            <input type="text" name="Precio" placeholder="$" className="form-control"/>
         </div>
         <div className="col-6 form-group p-4">
             <label>Stock disponible</label>
@@ -28,12 +30,12 @@ return(<div className="container" >
             <label>Descripcion</label>
             <textarea name="descripcion" placeholder="Breve descripcion de tu producto" className="form-control"/>
         </div>
-        <div className="col-sm-6 form-group p-5">
-            <label>Sube tu foto</label>
+        <div className="col-sm-12 form-group p-4">
+            <label className="alert alert-info" role="alert">Selecciona la foto para tu producto</label><br></br><br></br>
             <input type="file" name="document" className="form-control-file"/>
         </div>
         <div className="col-12 form-group">
-            <button className="btn btn-primary">Upload</button>
+            <button className="btn btn-primary">Subir</button>
         </div>
     </div>
 </form></div>)
