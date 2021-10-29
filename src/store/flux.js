0cfc35b5f60713306a1f4a776ff0c6f5a0dfbc4a
sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-10-29 16:06:10 -0300

    Terminadas tablas e inputs Sprint 2

--- src/store/flux.js
+++ src/store/flux.js
@@ -5,7 +5,8 @@ export const getState = ({setStore, getStore, getActions}) => {
 
     return {
         store: {
-            login: {rut:"", password:""}
+            login: {rut:"", password:""},
+            product: {item_title:"", item_description:"", item_stock:"", item_price:"", category:"", file:""}
         },
         actions: {
             getLogin: () => {
@@ -72,13 +73,41 @@ export const getState = ({setStore, getStore, getActions}) => {
             for (let i in input.files) {
                 data.append('file', input.files[i])
             }
-            /* data.append('file', input.files) */
             data.append('user', 'hubot')
             console.log(data)
             fetch('http://localhost:8080/post', {
             "mode": "no-cors",
             method: 'POST',
             body: data
+            })
+        },
+        getProduct: () => {
+            const store = getStore();
+            fetch('http://localhost:8080/product', {
+                "mode": "no-cors",
+                method: "POST",
+                headers: {
+            "Content-Type": "application/json",
+            "Accept": 'application/json',
+        },
+        body: JSON.stringify(store.product)
+        })
+        .then(resp => {
+            return resp.json();
+        })
+        .then(data => {
+            console.log("Posteado",data);
+        })
+        .catch(error => {
+
+             console.log(error);
+        })
+        },
+
+        handleChangeProduct: (e) => {
+            const {product} = getStore();
+            setStore({
+                product: { ...product, [e.target.name]: e.target.value }
             })
         },
 
--- src/views/Post.js
+++ src/views/Post.js
@@ -5,7 +5,7 @@ import '../styles/Post.css'
 
 const Post = () => {
 
-    const {actions} = useContext(Context)
+    const {store, actions} = useContext(Context)
 
 return(<div className="container" >
         <img src={logo} className="img-fluid w-50 m-4" alt="kickstart"/><br></br><br></br>
@@ -13,28 +13,28 @@ return(<div className="container" >
     <div className="row">
         <div className="col-6 form-group p-4">
             <label>Nombre del producto</label>
-            <input type="text" name="producto" placeholder="Producto" className="form-control"/>
+            <input type="text" name="item_title" placeholder="Producto" className="form-control" required onChange={actions.handleChangeProduct} value={store.product.item_title}/>
         </div>
         <div className="col-6 form-group p-4">
             <label>Categoria</label>
-            <input type="text" name="categoria" placeholder="Categoria" className="form-control"/>
+            <input type="text" name="category" placeholder="Categoria" className="form-control" required onChange={actions.handleChangeProduct} value={store.product.category}/>
         </div>
         <div className="col-6 form-group p-4">
             <label>Precio</label>
-            <input type="text" name="Precio" placeholder="$" className="form-control"/>
+            <input type="text" name="item_price" placeholder="$" className="form-control" required onChange={actions.handleChangeProduct} value={store.product.item_price}/>
         </div>
         <div className="col-6 form-group p-4">
             <label>Stock disponible</label>
-            <input type="text" name="precio" placeholder="Unidades en stock" className="form-control"/>
+            <input type="text" name="item_stock" placeholder="Unidades en stock" className="form-control" required onChange={actions.handleChangeProduct} value={store.product.item_stock}/>
         </div>
         <div className="col-12 form-group">
             <label>Descripcion</label>
-            <textarea name="descripcion" placeholder="Breve descripcion de tu producto" className="form-control"/>
+            <textarea name="item_description" placeholder="Breve descripcion de tu producto" className="form-control" required onChange={actions.handleChangeProduct} value={store.product.item_description}/>
         </div>
         <div className="col-sm-12 form-group p-4 ">
             <label className="alert alert-info" role="alert">Selecciona las fotos para tu producto. Puedes subir un maximo de 5 fotos</label>
             <input type="file" name="file" className="col-sm-9 form-control-file btn btn-outline-secondary" multiple/>
-            <button className="col-sm-2 btn btn-primary m-2">Subir</button>
+            <button className="col-sm-2 btn btn-primary m-2" onClick={actions.getProduct}>Subir</button>
         </div>
     </div>
 </form></div>)
